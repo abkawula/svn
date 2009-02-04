386ed4beda5a214e627b06a2cf19b08f43403da1
adam <adam@ff39be47-d5b4-4cb0-9442-9dfee3c5ba36>
2009-02-04 22:24:53 +0000

    properly integrated hibernate with project

--- BeerLogger/src/main/java/web/BrowseController.java
+++ BeerLogger/src/main/java/web/BrowseController.java
@@ -1,29 +1,18 @@
 package web;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import domain.Recipe;
-import domain.Recipe.Style;
 
 @Controller
-public class BrowseController extends MultiActionController {
+public class BrowseController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public void showCategories(Model model) {
+	public void showCategories() {
 		System.out.println("//////////////////////////////////////////////////////");
-		model.addAttribute("style", Recipe.Style.values());
+		
 	}
-	
-	
-	
-
-    
-
 }
